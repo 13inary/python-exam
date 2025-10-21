2438df74bb25e03bcba05126bfa6bba463e865a2
iamclient <936095897@qq.com>
2025-10-21 17:09:33 +0800

    template: 处理缺失数据的报错问题

--- 2_data/string/template.py
+++ 2_data/string/template.py
@@ -7,4 +7,16 @@ new_str = template.format_map(src_map)
 print("template str:", new_str, "\n") # Name: Alice, Age: 30
 
 
+# 处理map没有模板需要的数据的报错问题
+# from collections import defaultdict
+class SafeDict(dict):
+    def __missing__(self, key):
+        return f"<{key} missing>"
+
+data = SafeDict({"city": "Paris"})
+print("type of data:", type(data))
+print("data:", data)
+print("{city}, {country}".format_map(data))  # 输出：Paris, <country missing>
+
+
 
