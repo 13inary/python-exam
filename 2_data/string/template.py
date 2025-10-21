e48d1d5bbf9db0346fdabf527208b93e6fb40fff
iamclient <936095897@qq.com>
2025-10-21 17:04:16 +0800

    fix

--- 2_data/string/template.py
+++ 2_data/string/template.py
@@ -1,5 +1,6 @@
 # 定义一个字符串共用模板，里面有变化的数据使用 {xxx} 占位，使用 format 方法传入数据
 
+# map数据填充模板中的空位，这里map需要具备模板需要的所有数据，否则会报错
 template = "Name: {name}, Age: {age}"
 src_map = {"name": "Alice", "age": 30}
 new_str = template.format_map(src_map)
